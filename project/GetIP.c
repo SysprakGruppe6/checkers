59859f442f1198cb3a058746e2b4366f23969156
Mathias <magoe1997@gmail.com>
2018-11-26 16:49:32 +0100

    Testfile für DNS auflösung

---
+++ project/GetIP.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <string.h>
+
+
+int main(int argc, char * argv[]){
+
+  //Schleife für Kommandozeilenparameter
+
+    }
