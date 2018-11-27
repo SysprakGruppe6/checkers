b4ee8e78c41c56457ab6287cd83ebd0346db3859
Mathias <magoe1997@gmail.com>
2018-11-27 11:20:16 +0100

    Gitignore angepasst

--- project/test.c
+++ project/test.c
@@ -12,11 +12,11 @@
 #define HOSTNAME "sysprak.priv.lab.nm.ifi.lmu.de"
 int main(int argc, char * argv[])
 {
-    
+
     //Game-id und Spielernummer
     char *g;
     char *p;
-    
+
     //Schleife für Kommandozeilenparameter
     int i = 1;
     while (i < argc) {
@@ -28,39 +28,38 @@ int main(int argc, char * argv[])
         }
         i++;
     }
-    
+
     //Socketvariablen
     struct sockaddr_in sa;
     int res;
     int SocketFD;
-    
+
     //Socket
     SocketFD = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (SocketFD == -1) {
         perror("cannot create socket");
         exit(EXIT_FAILURE);
     }
-    
-    
+
+
     //Speichershit
     memset(&sa, 0, sizeof sa);
-    
+
     sa.sin_family = AF_INET;
     sa.sin_port = htons(PORTNUMBER);
     res = inet_pton(AF_INET, "10.155.92.35", &sa.sin_addr);                   //konvertiert die ip und speichert sie in &sa.sin_addr
-    
+
     //testet die connection
     if (connect(SocketFD, (struct sockaddr *)&sa, sizeof sa) == -1) {
         perror("connect failed");
         close(SocketFD);
         exit(EXIT_FAILURE);
     }
-    
+
     /* perform read write operations ... */
-    
+
     shutdown(SocketFD, SHUT_RDWR);
-    
+
     close(SocketFD);
     return EXIT_SUCCESS;
 }
-
