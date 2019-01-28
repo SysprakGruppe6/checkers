56b016a8b91fab71784638241523130a61290d24
Johannes Schluesslhuber <schluesslhuber@starzlach.cip.ifi.lmu.de>
2019-01-28 15:23:05 +0100

    added some functions to the ai

--- project/ai.c
+++ project/ai.c
@@ -19,6 +19,97 @@ void my_handler (int signum){
     }
 }
 
+ //Gibt den "Inhalt" einer Position aus
+ char getSpielstein(int pos, struct gds *game_data_struct_V2){
+   return game_data_struct_V2->spielfeld[pos][1];
+ }
+
+//Checkt ob wir auf eine von beiden Ränder sind
+int checkBorder(int pos){
+  if(pos%8-5==0)
+    return -1; //Linker Rand
+  if(pos%8-4==0)
+    return 1; //Rechter Rand
+  else
+    return 0;
+}
+
+//Checkt welcher spieler ein Spielstein gehört
+int getColour (char spielstein){
+  if (spielstein=='w'||spielstein=='W')//  1 = Weiss
+    return 1;
+  if (spielstein=='b'||spielstein=='B')// -1 = Schwarz
+    return -1;
+  else
+    return 0; //0 = Leer oder Fehler
+}
+
+//Gibt ein Array mit den möglichen gegnerische Steine aus
+char * getRivals(char spielstein){
+  char * result = malloc(sizeof(char)*2);
+  if(getColour(spielstein)==1){
+    result[0]='b';
+    result[1]='B';
+  }
+  if(getColour(spielstein)==-1){
+    result[0]='w';
+    result[1]='W';
+  }
+  else{
+    result[0]='*';
+    result[1]='*';
+  }
+  return result;
+}
+
+//Checkt ob die Stelle pos2 von einen gegnerischen Stein von pos1 besetzt ist
+int compare(int pos1, int pos2, struct gds *game_data_struct_V2){
+  char * rivals  = getRivals(getSpielstein(pos1, game_data_struct_V2));
+  char * ourTeam = getRivals(rivals[0]);
+  char spielstein2 = getSpielstein(pos2, game_data_struct_V2);
+  if(getSpielstein(pos1, game_data_struct_V2)=='*'||getSpielstein(pos2, game_data_struct_V2)=='*')//Check ob einer von beiden * ist. Evtll ist dann zeile 76/77 überflüssig, kA
+    return 0;
+  if (spielstein2==rivals[0]||spielstein2==rivals[1])//falls gegner auf pos2
+    return 1;
+  if (spielstein2==ourTeam[0]||spielstein2==ourTeam[1])//falls teammate auf pos2
+    return -1;
+  else
+    return 0;
+}
+
+//Checkt ob Spielstein an pos1 den Spielstein an pos2 schlagen kann. Falls ja, returnt es die neue Position von pos1 Stein, sonnst 0.
+// int strikeCheck(int pos1, int pos2){
+//   switch(getSpielstein(pos1))){
+//     case 'w':
+//       switch (checkBorder(pos1)) {
+//         case -1:
+//           if(pos2==pos1-4 && getSpielstein(pos1-7)=='*')
+//             return pos1-7;
+//           else
+//             return 0;
+//         case 1:
+//           if(pos2==pos1-4 && getSpielstein(pos1-9)=='*')
+//             return pos1-9;
+//           else
+//             return 0;
+//         case 0:
+//           if (checkBorder(pos2)==1||checkBorder(pos2)==-1)
+//             return 0;
+//           if(pos2==pos1-4 && getSpielstein(pos1-9)=='*')
+//       }
+//     case 'b':
+//
+//     case 'W':
+//
+//     case 'B':
+//
+//     case '*':
+//       return 0;
+//     default:
+//       return 0;
+//   }
+// }
+
 void think(struct gds *game_data_struct_V2){
 	printf("Am denken ...\n");
     //Test-Spielzug
